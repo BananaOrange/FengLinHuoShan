5bb05e4ed80277d14b5cb8b4c9df35e71bfb2880
BananaOrange <785077053@qq.com>
2019-11-28 01:07:38 +0800

    尝试修复注释模板和缩进的问题

--- src/chess/command/Dispatcher.java
+++ src/chess/command/Dispatcher.java
@@ -1,21 +1,21 @@
 package chess.command;
 
 /**
- * @description	分发类,将界面指令分发给command包中对应处理类 
- * @author  	BananaOrange
- * @date		2019年11月24日
+ * @description	    分发类,将界面指令分发给command包中对应处理类 
+ * @author        BananaOrange
+ * @date          2019年11月28日
  */
 public class Dispatcher {
 	/*
 	 * 指令分发
 	 */
-	public static void dispatchCommand(String command) {
-		try {
-			if(command.startsWith("ucci")) {
-				CommandUcci.handleCommand(command);
-			}
-		}catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
+    public static void dispatchCommand(String command) {
+        try {
+            if(command.startsWith("ucci")) {
+                CommandUcci.handleCommand(command);
+            }
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
--- src/chess/main/Launcher.java
+++ src/chess/main/Launcher.java
@@ -5,20 +5,20 @@ import java.util.Scanner;
 import chess.command.Dispatcher;
 
 /**
- * @description	程序入口
- * @author  	BananaOrange
- * @date		2019年11月24日
+ * @description	    程序入口
+ * @author        BananaOrange
+ * @date          2019年11月28日
  */
 public class Launcher {
-	public static void main(String[] args) {
+    public static void main(String[] args) {
         try {
             Scanner in = new Scanner(System.in);
             while(in.hasNext()){
-	            Dispatcher.dispatchCommand(in.next());
-	        }
-	        in.close();
-		}catch(Exception e) {
-			e.printStackTrace();
-		}
-	}
+                Dispatcher.dispatchCommand(in.next());
+            }
+            in.close();
+        }catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
